f6ff11266c394cfd3f5aa7e6f5944ba361f7ff72
Rylov Vlad <vlad.rulov@yandex.ru>
2022-08-13 22:46:17 +0500

    change request cities

--- myapp/controllers /bot.controller.js
+++ myapp/controllers /bot.controller.js
@@ -1,4 +1,6 @@
 const axios = require('axios').default
+const tokenMy = vk1.a.On14TtijaODhlGJBwgdQZ9m6PZqAWtx5HNW6BdTuPW5VtLDqpVKSwkbdzrFet4O6BDlhU9iaaQfyUkVRrXWcYJT_GL9m3V46SivvxqQJydbJXGumTFdDwzRMDnSTsuNjJTPa_b6dx0GsKW1wH6QVsApjD5Hx41Hqq3smrsfobifNMgVF1VkMHCWeK0edWgp_
+const tokenGroup = vk1.a.iR5VIpjVNSCmTUIT6CMA5Hkd4Woz8eO3O79ZYbfKsuuu2BIltM9gxPtop5VmRHfkgzR4BJjmFfD4gaaBK6ctWRQM_8KIkIFBEsyUQibq5i-p21R3sJlVVH27qpuDj9i-jqAYphQH-wHL3-l6FSDNMywX3soNXDcU4h5-v_Fq31n5Dtym-tz73UL0S8AE7fj6
 
 class BotController{
 
@@ -14,7 +16,12 @@ class BotController{
     async post(req, res, next) {
         try{
             if (req.body.type === "message_new") {
-                axios.post("https://dev.vk.com/method/database.getCities").then(res => {
+                console.log(req.body)
+                const requestCities = {
+                    country_id: 1,
+                    q: req.body.text
+                }
+                axios.post("https://dev.vk.com/method/database.getCities", requestCities).then(res => {
                     console.log(res)
                 })
             } else {
